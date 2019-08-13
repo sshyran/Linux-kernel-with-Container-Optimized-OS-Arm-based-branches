ed95451a5621467973fafccfefe19f8a5290e37a
Ke Wu <mikewu@google.com>
2019-08-13 14:03:27 -0700

    UPSTREAM: writeback: only allow one inflight and pending full flush
    When someone calls wakeup_flusher_threads() or
    wakeup_flusher_threads_bdi(), they schedule writeback of all dirty
    pages in the system (or on that bdi). If we are tight on memory, we
    can get tons of these queued from kswapd/vmscan. This causes (at
    least) two problems:
    
    1) We consume a ton of memory just allocating writeback work items.
       We've seen as much as 600 million of these writeback work items
       pending. That's a lot of memory to pointlessly hold hostage,
       while the box is under memory pressure.
    
    2) We spend so much time processing these work items, that we
       introduce a softlockup in writeback processing. This is because
       each of the writeback work items don't end up doing any work (it's
       hard when you have millions of identical ones coming in to the
       flush machinery), so we just sit in a tight loop pulling work
       items and deleting/freeing them.
    
    Fix this by adding a 'start_all' bit to the writeback structure, and
    set that when someone attempts to flush all dirty pages. The bit is
    cleared when we start writeback on that work item. If the bit is
    already set when we attempt to queue !nr_pages writeback, then we
    simply ignore it.
    
    This provides us one full flush in flight, with one pending as well,
    and makes for more efficient handling of this type of writeback.
    
    Acked-by: Johannes Weiner <hannes@cmpxchg.org>
    Tested-by: Chris Mason <clm@fb.com>
    Reviewed-by: Jan Kara <jack@suse.cz>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    (cherry picked from commit aac8d41cd438f25bf3110fc6b98f1d16d7dbc169)
    Signed-off-by: Ke Wu <mikewu@google.com>
    
    BUG=b:136020425
    TEST=trybot
    
    Change-Id: Ifa3b2864e88a3eba48b943ace0966ffc0224e0f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/1753175
    Reviewed-by: Sean Paul <seanpaul@chromium.org>
    Reviewed-by: Ke Wu <mikewu@google.com>
    Commit-Queue: Ke Wu <mikewu@google.com>
    Tested-by: Ke Wu <mikewu@google.com>

--- include/linux/backing-dev-defs.h
+++ include/linux/backing-dev-defs.h
@@ -24,6 +24,7 @@ enum wb_state {
 	WB_registered,		/* bdi_register() was done */
 	WB_writeback_running,	/* Writeback is in progress */
 	WB_has_dirty_io,	/* Dirty inodes on ->b_{dirty|io|more_io} */
+	WB_start_all,		/* nr_pages == 0 (all) work pending */
 };
 
 enum wb_congested_state {
