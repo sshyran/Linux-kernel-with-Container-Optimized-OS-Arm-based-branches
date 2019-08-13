d93a4d83ca6e7a8c291c54c9b09a0a951186d09f
Ke Wu <mikewu@google.com>
2019-08-13 13:49:29 -0700

    BACKPORT: fs: kill 'nr_pages' argument from wakeup_flusher_threads()
    Everybody is passing in 0 now, let's get rid of the argument.
    
    Reviewed-by: Jan Kara <jack@suse.cz>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    (cherry picked from commit 9ba4b2dfafaa711b41cc2102b0e9a529f3981218)
    Signed-off-by: Ke Wu <mikewu@google.com>
    
    Conflicts:
    	mm/vmscan.c
    (Removed arugument "0" in wakeup_flusher_threads of mm/vmscan.c
    in a different place)
    
    BUG=b:136020425
    TEST=trybot
    
    Change-Id: I97fef75c7b6efb150e6470599277cda0943b8b46
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/1753169
    Reviewed-by: Ke Wu <mikewu@google.com>
    Commit-Queue: Ke Wu <mikewu@google.com>
    Tested-by: Ke Wu <mikewu@google.com>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -1888,7 +1888,7 @@ shrink_inactive_list(unsigned long nr_to_scan, struct lruvec *lruvec,
 	 * rate. Nudge the flusher threads in case they are asleep.
 	 */
 	if (stat.nr_unqueued_dirty == nr_taken)
-		wakeup_flusher_threads(0, WB_REASON_VMSCAN);
+		wakeup_flusher_threads(WB_REASON_VMSCAN);
 
 	/*
 	 * Legacy memcg will stall in page writeback so avoid forcibly
