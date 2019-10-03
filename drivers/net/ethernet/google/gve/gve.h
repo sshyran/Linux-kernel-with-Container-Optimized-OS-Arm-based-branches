0163ccc22114432e3412f2a4f24147c2faf19c9e
Catherine Sullivan <csully@google.com>
2019-10-03 12:19:53 -0700

    gve: Add support for dma_mask register
    Add the dma_mask register and read it to set the dma_masks.
    gve_alloc_page will alloc_page with:
     GFP_DMA if priv->dma_mask is 24,
     GFP_DMA32 if priv->dma_mask is 32.
    
    Patches Squashed into this patch:
    - gve: Add support for dma_mask register
    - Fix address used in dma_sync_single_for_cpu. (Partially)
    - gve: Fix GFP flags when allocing pages (Partially)
    
    BUG=b/153867289
    
    Change-Id: I2a38307722f3333e9fe4635bc734fa86f91b6979
    Reviewed-on: https://cos-review.googlesource.com/c/third_party/kernel/+/1563
    Reviewed-by: Vaibhav Rustagi <vaibhavrustagi@google.com>

--- drivers/net/ethernet/google/gve/gve.h
+++ drivers/net/ethernet/google/gve/gve.h
@@ -236,6 +236,9 @@ struct gve_priv {
 	struct work_struct service_task;
 	unsigned long service_task_flags;
 	unsigned long state_flags;
+
+	/* Gvnic device's dma mask, set during probe. */
+	u8 dma_mask;
 #if (LINUX_VERSION_CODE < KERNEL_VERSION(4,10,0))
 	int max_mtu;
 #endif /* LINUX_VERSION_CODE < KERNEL_VERSION(4,10,0) */
@@ -466,7 +469,7 @@ static inline bool gve_can_recycle_pages(struct net_device *dev)
 /* buffers */
 int gve_alloc_page(struct gve_priv *priv, struct device *dev,
 		   struct page **page, dma_addr_t *dma,
-		   enum dma_data_direction);
+		   enum dma_data_direction, gfp_t gfp_flags);
 void gve_free_page(struct device *dev, struct page *page, dma_addr_t dma,
 		   enum dma_data_direction);
 /* tx handling */
