e4d6894a842d84c86103038f220a42683aaf354d
Song Liu <songliubraving@fb.com>
2021-02-25 15:43:14 -0800

    UPSTREAM: bpf: Enable task local storage for tracing programs
    To access per-task data, BPF programs usually creates a hash table with
    pid as the key. This is not ideal because:
     1. The user need to estimate the proper size of the hash table, which may
        be inaccurate;
     2. Big hash tables are slow;
     3. To clean up the data properly during task terminations, the user need
        to write extra logic.
    
    Task local storage overcomes these issues and offers a better option for
    these per-task data. Task local storage is only available to BPF_LSM. Now
    enable it for tracing programs.
    
    Unlike LSM programs, tracing programs can be called in IRQ contexts.
    Helpers that access task local storage are updated to use
    raw_spin_lock_irqsave() instead of raw_spin_lock_bh().
    
    Tracing programs can attach to functions on the task free path, e.g.
    exit_creds(). To avoid allocating task local storage after
    bpf_task_storage_free(). bpf_task_storage_get() is updated to not allocate
    new storage when the task is not refcounted (task->usage == 0).
    
    Signed-off-by: Song Liu <songliubraving@fb.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Acked-by: KP Singh <kpsingh@kernel.org>
    Acked-by: Martin KaFai Lau <kafai@fb.com>
    Link: https://lore.kernel.org/bpf/20210225234319.336131-2-songliubraving@fb.com
    (cherry picked from commit a10787e6d58c24b51e91c19c6d16c5da89fcaa4b)
    Signed-off-by: Brendan Jackman <jackmanb@google.com>
    
    SOURCE=UPSTREAM(a10787e6d58c24b51e91c19c6d16c5da89fcaa4b)
    BUG=b:217368493
    
    Change-Id: If50656e58bde153ca2662422294a8c2d5ec321ec

--- include/linux/bpf.h
+++ include/linux/bpf.h
@@ -1487,6 +1487,7 @@ struct bpf_prog *bpf_prog_by_id(u32 id);
 struct bpf_link *bpf_link_by_id(u32 id);
 
 const struct bpf_func_proto *bpf_base_func_proto(enum bpf_func_id func_id);
+void bpf_task_storage_free(struct task_struct *task);
 
 static inline bool unprivileged_ebpf_enabled(void)
 {
@@ -1686,6 +1687,10 @@ static inline const struct bpf_func_proto *
 bpf_base_func_proto(enum bpf_func_id func_id)
 {
 	return NULL;
+}
+
+static inline void bpf_task_storage_free(struct task_struct *task)
+{
 }
 
 static inline bool unprivileged_ebpf_enabled(void)
@@ -1891,6 +1896,8 @@ extern const struct bpf_func_proto bpf_per_cpu_ptr_proto;
 extern const struct bpf_func_proto bpf_this_cpu_ptr_proto;
 extern const struct bpf_func_proto bpf_ktime_get_coarse_ns_proto;
 extern const struct bpf_func_proto bpf_get_socket_ptr_cookie_proto;
+extern const struct bpf_func_proto bpf_task_storage_get_proto;
+extern const struct bpf_func_proto bpf_task_storage_delete_proto;
 
 const struct bpf_func_proto *bpf_tracing_func_proto(
 	enum bpf_func_id func_id, const struct bpf_prog *prog);
