9ae345dc57898f9cc62b6b65c2881e64e1f819b3
Andrei Vagin <avagin@google.com>
2021-05-26 13:54:55 -0700

    kernel: return use_mm/unuse_mm functions
    use_mm/unuse_mm allow to change mm of user threads.
    
    In v5.8, they have been renamed to kthread_use_mm, kthread_unuse_mm and filled
    with kthread-specific code.
    
    The gVisor XEMU module uses use_mm/unuse_mm to change mm of user-threads.
    
    BUG=183648601
    TEST=Compiled and verified locally
    SOURCE=COS
    
    Fixes: f5678e7f2ac3 ("kernel: better document the use_mm/unuse_mm API contract")
    Signed-off-by: Andrei Vagin <avagin@google.com>
    Change-Id: Ie3bcf33697d185ec8021254055b654bc1833c3b3
    Reviewed-on: https://cos-review.googlesource.com/c/third_party/kernel/+/17930
    Reviewed-by: Bhasker Hariharan <bhaskerh@google.com>
    Reviewed-by: Oleksandr Tymoshenko <ovt@google.com>
    Tested-by: Oleksandr Tymoshenko <ovt@google.com>

--- kernel/kthread.c
+++ kernel/kthread.c
@@ -1257,12 +1257,11 @@ EXPORT_SYMBOL(kthread_destroy_worker);
  * kthread_use_mm - make the calling kthread operate on an address space
  * @mm: address space to operate on
  */
-void kthread_use_mm(struct mm_struct *mm)
+void use_mm(struct mm_struct *mm)
 {
 	struct mm_struct *active_mm;
 	struct task_struct *tsk = current;
 
-	WARN_ON_ONCE(!(tsk->flags & PF_KTHREAD));
 	WARN_ON_ONCE(tsk->mm);
 
 	task_lock(tsk);
@@ -1283,6 +1282,16 @@ void kthread_use_mm(struct mm_struct *mm)
 
 	if (active_mm != mm)
 		mmdrop(active_mm);
+}
+EXPORT_SYMBOL_GPL(use_mm);
+
+void kthread_use_mm(struct mm_struct *mm)
+{
+	struct task_struct *tsk = current;
+
+	WARN_ON_ONCE(!(tsk->flags & PF_KTHREAD));
+
+	use_mm(mm);
 
 	to_kthread(tsk)->oldfs = force_uaccess_begin();
 }
@@ -1297,9 +1306,17 @@ void kthread_unuse_mm(struct mm_struct *mm)
 	struct task_struct *tsk = current;
 
 	WARN_ON_ONCE(!(tsk->flags & PF_KTHREAD));
-	WARN_ON_ONCE(!tsk->mm);
-
 	force_uaccess_end(to_kthread(tsk)->oldfs);
+
+	unuse_mm(mm);
+}
+EXPORT_SYMBOL_GPL(kthread_unuse_mm);
+
+void unuse_mm(struct mm_struct *mm)
+{
+	struct task_struct *tsk = current;
+
+	WARN_ON_ONCE(!tsk->mm);
 
 	task_lock(tsk);
 	sync_mm_rss(mm);
@@ -1310,7 +1327,7 @@ void kthread_unuse_mm(struct mm_struct *mm)
 	local_irq_enable();
 	task_unlock(tsk);
 }
-EXPORT_SYMBOL_GPL(kthread_unuse_mm);
+EXPORT_SYMBOL_GPL(unuse_mm);
 
 #ifdef CONFIG_BLK_CGROUP
 /**
