69bfa6829258b9e3f066963f0411e271b6d59223
Oleksandr Tymoshenko <ovt@google.com>
2021-08-27 05:02:10 +0000

    Pull container from gcr.io before using it

--- kokoro/kokoro_build.sh
+++ kokoro/kokoro_build.sh
@@ -3,5 +3,6 @@
 set -ex
 
 cd "${KOKORO_ARTIFACTS_DIR}/git/kernel"
+sudo gcloud docker -- pull gcr.io/cloud-kernel-build/cos-kernel-devenv
 sudo docker run --rm \
   -v $(pwd):/src -w /src gcr.io/cloud-kernel-build/cos-kernel-devenv -k
