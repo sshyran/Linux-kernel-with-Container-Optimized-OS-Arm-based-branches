7b300b5dde8f6f90d71a1ce611becb1f9e2a6789
Oleksandr Tymoshenko <ovt@google.com>
2021-09-03 22:17:33 +0000

    troubleshoot GCR access permissions

--- kokoro/kokoro_build.sh
+++ kokoro/kokoro_build.sh
@@ -6,6 +6,7 @@ cd "${KOKORO_ARTIFACTS_DIR}/git/kernel"
 env | sort
 uname -a
 which gcloud
+sudo gcloud auth list
 sudo gcloud docker -- pull gcr.io/cloud-kernel-build/cos-kernel-devenv
 sudo docker run --rm \
   -v $(pwd):/src -w /src gcr.io/cloud-kernel-build/cos-kernel-devenv -k
