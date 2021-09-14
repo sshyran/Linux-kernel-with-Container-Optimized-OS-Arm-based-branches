942a78933cec2f376eb75060ae0f6c51bc48137c
Oleksandr Tymoshenko <ovt@google.com>
2021-09-14 05:13:59 +0000

    Add build number as a localversion part

--- kokoro/kokoro_build.sh
+++ kokoro/kokoro_build.sh
@@ -8,5 +8,7 @@ uname -a
 which gcloud
 sudo gcloud auth list
 sudo gcloud docker -- pull gcr.io/cloud-kernel-build/cos-kernel-devenv
+pwd
+echo -n "-cos${KOKORO_BUILD_NUMBER}" > localversion
 sudo docker run --rm \
   -v $(pwd):/src -w /src gcr.io/cloud-kernel-build/cos-kernel-devenv -k
