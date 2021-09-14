732d8090e788a434cd32eb701e1814230642d7d0
Oleksandr Tymoshenko <ovt@google.com>
2021-09-14 23:44:49 +0000

    experiment with keeping artifacts

--- kokoro/kokoro_build.sh
+++ kokoro/kokoro_build.sh
@@ -10,5 +10,14 @@ sudo gcloud auth list
 sudo gcloud docker -- pull gcr.io/cloud-kernel-build/cos-kernel-devenv
 pwd
 echo -n "-cos${KOKORO_BUILD_NUMBER}" > localversion
+# Remove '+' sign from the version
+touch .scmversion
 sudo docker run --rm \
-  -v $(pwd):/src -w /src gcr.io/cloud-kernel-build/cos-kernel-devenv -k
+  -u "$(id -u):$(id -g)" \
+  -v $(pwd):/src -w /src \
+  gcr.io/cloud-kernel-build/cos-kernel-devenv -k
+
+# XXX: replace with `make kernelversion > version.full` in the containers
+KERNEL_VERSION="$(ls linux-*.tar.xz | sed s@linux-@@ | sed -E s@-[^-]*.tar.xz@@)"
+
+gsutil cp linux-*.tar.xz "gs://ovt-dev/kernel-builds/${VERSION}/"
