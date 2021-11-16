4fd3e99c3bc75c4897e8268d0585314df0921b44
Meena Shanmugam <meenashanmugam@google.com>
2021-11-16 03:25:10 +0000

    Build arm64 as well.
    Change-Id: Ie9a3fa56f8443163538b287849501cc992254a72

--- kokoro/kokoro_build.sh
+++ kokoro/kokoro_build.sh
@@ -2,26 +2,27 @@
 
 set -ex
 
+CONTAINER_NAME="gcr.io/cloud-kernel-build/cos-kernel-devenv"
+CONTAINER_CMD="sudo docker run --rm -v $(pwd):/src -w /src ${CONTAINER_NAME} "
 cd "${KOKORO_ARTIFACTS_DIR}/git/kernel"
 env | sort
 uname -a
 which gcloud
 sudo gcloud auth list
-sudo gcloud docker -- pull gcr.io/cloud-kernel-build/cos-kernel-devenv
+sudo gcloud docker -- pull ${CONTAINER_NAME}
 pwd
 echo -n "-cos${KOKORO_BUILD_NUMBER}" > localversion
 # Remove '+' sign from the version
 touch .scmversion
-sudo docker run --rm \
-  -v $(pwd):/src -w /src \
-  gcr.io/cloud-kernel-build/cos-kernel-devenv -k
+for arch in 'x86' 'arm64'
+do
+  ${CONTAINER_CMD} -k -A ${arch}
 
-# Fixup permissions
-sudo chown -R "$(id -u):$(id -g)" .
+  # Fixup permissions
+  sudo chown -R "$(id -u):$(id -g)" .
 
-# XXX: replace with `make kernelversion > version.full` in the containers
-KERNEL_VERSION="$(ls linux-*.tar.xz | sed s@linux-@@ | sed -E s@-[^-]*.tar.xz@@)"
-KERNEL_VERSION_ARCH="$(ls linux-*.tar.xz | sed s@linux-@@ | sed -E s@.tar.xz@@)"
+  KERNEL_VERSION=`${CONTAINER_CMD} kernelrelease | tail -1`
 
-gsutil cp linux-*.tar.xz "gs://ovt-dev/kernel-builds/${KERNEL_VERSION}/"
-gsutil cp kernel/kheaders_data.tar.xz "gs://ovt-dev/kernel-builds/${KERNEL_VERSION}/linux-headers-${KERNEL_VERSION_ARCH}.tar.xz"
+  gsutil cp cos-kernel-${KERNEL_VERSION}-${arch}.txz "gs://ovt-dev/kernel-builds/${KERNEL_VERSION}/"
+  #gsutil cp kernel/kheaders_data.tar.xz "gs://ovt-dev/kernel-builds/${KERNEL_VERSION}/linux-headers-${KERNEL_VERSION_ARCH}.tar.xz"
+done
